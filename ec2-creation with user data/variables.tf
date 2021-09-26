b3bc8685e7f850eb2afae2e4b82ee30702abdcb9
kishore1919 <kishoreselvaraj19@gmail.com>
2021-09-26 13:29:34 +0530

    first commit

---
+++ ec2-creation with user data/variables.tf
@@ -0,0 +1,12 @@
+variable "ami" {
+    default = "ami-087c17d1fe0178315"
+}
+
+variable "cpu" {
+    description = "CPU and others"
+    default = "t2.micro"
+}
+variable "key" {
+    description = "Key information"
+    default = "Key02"
+}
